78e37552db5eaf476df971bca2bd39121eb4135f
Guy Gaziv <guyga@mit.edu>
2022-11-04 23:26:12 -0400

    Implement padding color frame to images

--- gputils/image_utils.py
+++ gputils/image_utils.py
@@ -6,6 +6,7 @@
 import gputils.startup_guyga as gputils
 from skimage import io as skio
 from imutils import build_montages
+import numpy as np
 
 
 def make_montage(imgs, n_col=None):
@@ -23,3 +24,14 @@ def get_images(images_paths, n_threads=20):
     with gputils.PoolReported(n_threads) as pool:
         res_list = pool.map(lambda image_path: skio.imread(image_path), images_paths)
     return np.array(res_list)
+
+
+def color_framed(img_rgb, pad_size, rgb):
+    """Pad image with colored frame
+    """
+    if pad_size == 0:
+        return img_rgb
+    dtype = img_rgb.dtype
+    img = np.pad(img_rgb, (*[[pad_size] * 2]*2, (0,0)))
+    img[:pad_size] = img[-pad_size:] = img[:, -pad_size:] = img[:, :pad_size] = rgb
+    return img.astype(dtype)
