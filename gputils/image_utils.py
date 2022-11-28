b011946c3110b0ecd4b8c11fa8828f89c6a43005
Guy Gaziv <guyga@mit.edu>
2022-11-27 22:11:58 -0500

    Implement pilify and on-image labeling

--- gputils/image_utils.py
+++ gputils/image_utils.py
@@ -7,6 +7,7 @@ import gputils.startup_guyga as gputils
 from skimage import io as skio
 from imutils import build_montages
 import numpy as np
+from PIL import ImageFont, ImageDraw
 
 
 def make_montage(imgs, n_col=None):
@@ -35,3 +36,34 @@ def color_framed(img_rgb, pad_size, rgb):
     img = np.pad(img_rgb, (*[[pad_size] * 2]*2, (0,0)))
     img[:pad_size] = img[-pad_size:] = img[:, -pad_size:] = img[:, :pad_size] = rgb
     return img.astype(dtype)
+
+
+def pilify(a):
+    """Get/convert to a PIL image"""
+    if isinstance(a, np.ndarray):
+        if a.dtype != 'uint8':
+            a = (a * 255).astype('uint8')
+    else:
+        raise NotImplementedError
+    return gputils.Image.fromarray(a)
+
+
+class FrameLabeler():
+    def __init__(self, color=(255,255,255), xy=(5, 5),
+                 font=ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf", 16)):
+        self.font = font
+        self.color = color
+        self.xy = xy
+            
+    def __call__(self, img_pil, label):
+        img_pil = pilify(img_pil)
+        draw = ImageDraw.Draw(img_pil)
+        draw.text(self.xy, label , self.color, font=self.font)
+        return img_pil
+    
+    
+# def save_gif(frames_pil, filepath, duration=100, loop=1):
+#     """Images don't look good"""
+#     frames = [gputils.Image.fromarray(arr) for arr in frames_pil]
+#     frames[0].save(filepath, format="GIF", append_images=frames, 
+#                    save_all=True, duration=duration, loop=loop)
